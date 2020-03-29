9057e49626c0c5e538fafc2bd977365568136026
glebkiselev <12bny23gleb@gmail.com>
2020-03-29 15:13:07 +0300

    validation pipe for signup

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,17 @@
+import { IsString, MinLength, MaxLength, Matches } from "class-validator";
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(24)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(24)
+  @Matches(
+    /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z)(?=.*[a-z]).*$/, 
+    { message: 'password too weak' },
+  )
   password: string;
 }
