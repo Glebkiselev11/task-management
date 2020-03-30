b399216a3ef6944b2d53e50f52e9109c53ba63c1
glebkiselev <12bny23gleb@gmail.com>
2020-03-30 17:11:55 +0300

    auth updating a user's task status

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -39,13 +39,14 @@ export class TasksController {
     return this.tasksService.createTask(createTaskDto, user);
   }
 
-  // @Patch(':id/status')
-  // updateTaskStatus(
-  //   @Param('id', ParseIntPipe) id: number, 
-  //   @Body('status', TaskStatusValidationPipe) status: TaskStatus 
-  // ): Promise<Task> {
-  //   return this.tasksService.updateTaskStatus(id, status);
-  // }
+  @Patch(':id/status')
+  updateTaskStatus(
+    @Param('id', ParseIntPipe) id: number, 
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+    @GetUser() user: User,
+  ): Promise<Task> {
+    return this.tasksService.updateTaskStatus(id, status, user);
+  }
 
   @Delete(':id')
   deleteTaskById(@Param('id') id: number): Promise<void> {
