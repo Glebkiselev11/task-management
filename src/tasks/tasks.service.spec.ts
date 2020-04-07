0db41ae0167feb93442d696518623c592bb48899
glebkiselev <12bny23gleb@gmail.com>
2020-04-07 14:12:19 +0300

    spec for deleteTask

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -11,6 +11,7 @@ const mockTaskRepository = () => ({
   getTasks: jest.fn(),
   findOne: jest.fn(),
   createTask: jest.fn(),
+  delete: jest.fn()
 });
 
 describe('TasksService', () => {
@@ -75,5 +76,21 @@ describe('TasksService', () => {
 
       expect(result).toEqual('someTask');
     });
+  });
+
+  describe('deleteTask', () => {
+    it('calls taskRepository.delete() to delete a task', async () => {
+      taskRepository.delete.mockResolvedValue({affected: 1});
+
+      expect(taskRepository.delete).not.toHaveBeenCalled();
+      await tasksService.deleteTask(1, mockUser);
+      expect(taskRepository.delete).toHaveBeenCalledWith({ id: 1, userId: mockUser.id});
+    });
+
+    it('throws an error as task could not be found', async () => {
+      taskRepository.delete.mockResolvedValue({affected: 0});
+      await expect(tasksService.deleteTask(1, mockUser)).rejects.toThrow(NotFoundException);
+    });
+
   });
 });
