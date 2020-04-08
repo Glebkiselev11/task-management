4db4c9730f05cd9ef75f6c19d9f1198a731c5c22
glebkiselev <12bny23gleb@gmail.com>
2020-04-08 19:41:16 +0300

    spec for validateUserPassword

--- src/auth/user.repository.spec.ts
+++ src/auth/user.repository.spec.ts
@@ -1,6 +1,7 @@
 import { Test } from "@nestjs/testing";
 import { UserRepository } from "./user.repository";
 import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import { User } from "./user.entity";
 
 const mockCredentialsDto = { username: 'TestUsername', password: 'TestPassword' };
 
@@ -40,5 +41,40 @@ describe('UserRepository', () => {
       await expect(userRepository.signUp(mockCredentialsDto)).rejects.toThrow(InternalServerErrorException);
     });
 
+  });
+
+  describe('validateUserPassword', () => {
+    let user;
+
+    beforeEach(() => {
+      userRepository.findOne = jest.fn();
+      user = new User();
+      user.username = 'TestUsername';
+      user.validatePassword = jest.fn();
+    })
+
+    it('returns the username as validation is successful', async () => {
+      userRepository.findOne.mockResolvedValue(user);
+      user.validatePassword.mockResolvedValue(true);
+
+      const result = await userRepository.validateUserPassword(mockCredentialsDto);
+      expect(result).toEqual('TestUsername');
+    });
+
+    it('returns null as user cannot be found', async () => {
+      userRepository.findOne.mockResolvedValue(null);
+      const result = await userRepository.validateUserPassword(mockCredentialsDto);
+      expect(user.validatePassword).not.toHaveBeenCalled();
+      expect(result).toBeNull();
+    });
+
+    it('returns null as password is invalid', async () => {
+      userRepository.findOne.mockResolvedValue(user);
+      user.validatePassword.mockResolvedValue(false);
+
+      const result = await userRepository.validateUserPassword(mockCredentialsDto);
+      expect(user.validatePassword).toHaveBeenCalled();
+      expect(result).toBeNull();
+    });
   });
 });
